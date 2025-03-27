3d90b6dbd71bd1d25e58837c40ede535fba28db5
Ega Bagus Purnama <egabagus1@gmail.com>
2025-03-27 16:57:49 +0700

    Feat (Add and Edit Skill)

---
+++ app/Enums/SkillLevelEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum SkillLevelEnum: string
+{
+    case BEGINNER = 'Beginner';
+    case COMPETENT = 'Competent';
+    case PROFICIENT = 'Proficient';
+    case EXPERT = 'Expert';
+}
--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SkillRequest;
+use App\Http\Resources\Api\ApiError;
+use App\Http\Resources\Api\ApiSuccess;
 use App\Http\Resources\SkillResource;
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class SkillController extends Controller
 {
@@ -12,5 +17,45 @@ class SkillController extends Controller
     {
         $data = Skill::paginate($request->per_page ?? 10);
         return SkillResource::collection($data);
+    }
+
+    public function store(SkillRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            $payload = [
+                'name' => $request->name,
+                'description' => $request->description,
+                'level' => $request->level,
+            ];
+
+            $newSkill = Skill::create($payload);
+
+            DB::commit();
+            return (new ApiSuccess())($newSkill);
+        } catch (Throwable $th) {
+            DB::rollBack();
+            return (new ApiError())($th->getMessage());
+        }
+    }
+
+    public function update(int $id, SkillRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            $payload = [
+                'name' => $request->name,
+                'description' => $request->description,
+                'level' => $request->level,
+            ];
+
+            $update = Skill::find($id)->update($payload);
+
+            DB::commit();
+            return (new ApiSuccess())($update);
+        } catch (Throwable $th) {
+            DB::rollBack();
+            return (new ApiError())($th->getMessage());
+        }
     }
 }
